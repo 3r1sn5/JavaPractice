76bdd75a9e9c0fad7c95490f48ed8587c8e4a8bb
ErlsnWu <a0987567908@gmail.com>
2022-10-06 00:40:33 +0800

    Homework1

---
+++ Homework1/HW.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.*;
+
+public class HW
+{
+    public static void main(String[] args) throws IOException
+    {
+        System.out.print("Welcome To Java.");
+        System.out.print("This is Page 9.\r\n\r\n");
+        //Page 9
+
+        System.out.println("Welcome To Java.");
+        System.out.println("This is Page 10\r\n");
+        //Page 10
+
+        System.out.println('S');
+        System.out.println("String");
+        System.out.println(5 + "\r\n");
+        //Page 16
+
+        System.out.println("Show backslash : \\");
+        System.out.println("Show apostrophe : \'");
+        System.out.println("Character of octal 101 : \101");
+        System.out.println("Character of hexadecimal 0061 : \u0061 \r\n");
+        //Page 18
+
+        int num;
+        num = 5;
+        System.out.println("Value of variable num : "+ num+"\r\n");
+        //Page 25
+
+        num = 10;
+        System.out.println("Reassign value of num.");
+        System.out.println("Num after reassign : "+ num+"\r\n");
+        //Page 26
+
+        int num1 = 10, num2;
+        System.out.println("Value of num1 : "+ num1);
+        num2 = num1;
+        System.out.println("Assign Num1 to num2.");
+        System.out.println("Value of num2 : "+ num2 + "\r\n");
+        //Page 27
+
+        System.out.println("Input a integer : ");
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br1.readLine();
+        int num3 = Integer.parseInt(str1);
+        System.out.println("Output : "+ num3);
+        System.out.println("Input a string : ");
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+        String str2 = br2.readLine();
+        System.out.println("Output : " + str2 + "\r\n");
+        //Page 28
+    }
+}
---
+++ Homework1/HW2.java
@@ -0,0 +1,50 @@
+public class HW2 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1 + 2 = " + (1 + 2));
+        System.out.println("3 + 4 = " + (3 + 4));
+        int num1 = 2;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("Value of num1 : " + num1);
+        System.out.println("Value of num2 : " + num2);
+        System.out.println("num1 + num2 = " + sum);
+        num1 = num1 + 1;
+        System.out.println("num1 + 1 = " + num1 + "\r\n");
+        //Page 31
+
+        int num3 = 10;
+        int num4 = 5;
+        System.out.println("num3 & num4 operation : ");
+        System.out.println("num3 + num4 = " + (num3 + num4));
+        System.out.println("num3 - num4 = " + (num3 - num4));
+        System.out.println("num3 * num4 = " + (num3 * num4));
+        System.out.println("num3 / num4 = " + (num3 / num4));
+        System.out.println("num3 % num4 = " + (num3 % num4) + "\r\n");
+        int a =0, b = 0, c = 0;
+        b = a++;
+        c = ++a;
+        System.out.println("+1 after assign, b = " + b);
+        System.out.println("+1 before assign, c = " + c + "\r\n");
+        //Page 34
+
+        double dnum = 555.5;
+        System.out.println("Double dnum : " + dnum);
+        System.out.println("Assign dnum to integer inum.");
+        int inum = (int) dnum;
+        System.out.println("inum = " + inum + "\r\n");
+        //Page38
+
+        int d = 2;
+        double pi = 3.14159;
+        System.out.println("A circle with diameter " + d);
+        System.out.println("Circumference : " + (d * pi) + " cm");
+        int num5 = 5, num6 = 2;
+        double div1 = num5 / num6;
+        double div2 = (double)num5 / (double)num6;
+        System.out.println("5/2 = " + div1);
+        System.out.println("5/2 = " + div2);
+        //Page 39
+    }
+}
